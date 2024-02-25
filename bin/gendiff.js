465283aaaae6c54fe48cbcb28adedc9034fdd5b1
mker <krmichael25@gmail.com>
2024-02-25 11:58:05 +0600

    update help information

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
-const { program } = require('commander');
+import { Command } from 'commander';
+
+const program = new Command();
 
 program
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
-    .option('-V, --version', 'output the version number')
-    .option('-h, --help', 'output usage information')
+    .version('0.0.1', '-V, --version', 'output the version number')
+    .helpOption('-h, --help', 'output usage information');
 
+program.parse();
     
